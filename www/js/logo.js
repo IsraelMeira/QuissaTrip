e1b9a4e8e2157dcf4b98ed9ea3dba649c3f0a0f7
LuisFGS <luisfernandogs69@gmail.com>
2018-04-09 20:13:15 -0300

    Correção Logo

--- www/js/logo.js
+++ www/js/logo.js
@@ -1,11 +1,14 @@
 var modal = document.getElementById('myModal');
+var bool = window.sessionStorage.getItem('opened');
 var intervalo = window.setInterval(function() {
-    modal.style.display = "block";
+	if (bool != 'off') {
+    	modal.style.display = "block";
+    }
 }, 50);
 window.setTimeout(function() {
     clearInterval(intervalo);
     modal.style.display = "none";
+    window.sessionStorage.setItem('opened', 'off');
 }, 3000);
-window.onload = initPage;
 function intervalo(){
 }
